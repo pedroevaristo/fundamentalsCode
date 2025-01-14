8e911f59320bcc80c6ce302afd82fd7b49a9af19
Pedro Evaristo Dantas Zaranza <pedrodzprogramacao@gmail.com>
2025-01-13 21:59:41 -0300

    colocando os metodos na classe mamifero

--- src/Poo/herancaAnimal/Mamifero.java
+++ src/Poo/herancaAnimal/Mamifero.java
@@ -3,12 +3,18 @@ package Poo.herancaAnimal;
 public class Mamifero extends Animal {
 	private final boolean temPelos;
 	
-	public Mamifero(String nome, String raca, int idade) {
+	public Mamifero(String nome, String raca, int idade, boolean temPelos) {
 		super(nome, raca, idade);
+		this.temPelos = temPelos;
 		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean isTemPelos() {
+		return temPelos;//Getter para processar o atributo
 	}
 	
 	public void amamentar() {
 		System.out.println("Este mamifero pode amamenta.");
 	}
+	
 }
